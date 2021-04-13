ba0100404255152cec29e97ac3e5e430fa22e771
rahulvishwakarm <rahulvishwakarma6189@gmail.com>
2021-04-13 17:10:53 +0530

    Updated Basic List Component assigned key

--- react-basics/src/List and Keys/BasicListCompoent.js
+++ react-basics/src/List and Keys/BasicListCompoent.js
@@ -3,7 +3,7 @@ import React from 'react'
 function BasicListCompoent(props) {
     const number = props.numbers;
     const lsitItem = number.map((num) =>
-        <li>{num}</li>
+        <li key={number.toString()}>{num}</li>
     )
     return (
         <ul>{lsitItem}</ul>
